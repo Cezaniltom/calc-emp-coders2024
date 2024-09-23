a92bc0f8fae379d5d336a91e1b81e8edb1f3ac16
cezaniltom <cezaniltom@outlook.com>
2024-09-23 12:44:36 -0300

    Cria prompt da operacao desejada

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const num1 = parseInt(prompt('Digite o primeiro número'));
 const num2 = parseInt(prompt('Digite o segundo número'))
+const operacao = prompt('Escolha a operação: somar, subtrair, multiplicar, dividir').toLowerCase()
+
