b4dd1cefae517128624feafa24f31b5c3c6ec24f
cezaniltom <cezaniltom@outlook.com>
2024-09-23 12:45:30 -0300

    Cria a lógica da operacao somar no console

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const num1 = parseInt(prompt('Digite o primeiro número'));
 const num2 = parseInt(prompt('Digite o segundo número'))
 const operacao = prompt('Escolha a operação: somar, subtrair, multiplicar, dividir').toLowerCase()
 
+if(operacao === 'somar') {
+    console.log(num1 + num2)
+} 
